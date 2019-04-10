5a00d52760984132ed15b6bacbec3b1125eefb38
Saar Levin <saar.levin@forescout.com>
2019-04-10 10:12:44 +0300

    first commit

---
+++ Get-LastLoggedInUser.ps1
@@ -0,0 +1,17 @@
+﻿
+function Get-LastLoggedInUser {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [String]$ComputerName="LocalHost"
+    )
+
+    Begin {}
+    Process {
+        $LastLogOn = (Get-WmiObject -Class win32_process -ComputerName $ComputerName | Where-Object name -Match explorer).getowner().user    
+    }
+    End {
+        return $LastLogOn
+    }   
+}
