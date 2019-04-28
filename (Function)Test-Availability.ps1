d8470f9780b6aa64e4b8b380c588154296f4a4ab
Saar Levin <saar.levin@forescout.com>
2019-04-28 10:08:53 +0300

    First commit

---
+++ (Function)Test-Availability.ps1
@@ -0,0 +1,31 @@
+<#
+    .NOTES
+    ===========================================================================
+     Created on:    28.4.2019
+     Created by:    Saar Levin
+     Organization:
+    ===========================================================================
+#>
+
+function Test-Availability {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True,
+            ValueFromPipeline=$True,
+            ValueFromPipelineByPropertyName=$True)]
+        [string]$ComputerName
+    )
+    Begin {
+        $colStatus = @()
+    }
+    Process {
+        $obj = [PSCustomObject]@{
+            Computer = $ComputerName
+            Status    = "UP"
+        }
+        $check = Test-Connection -ComputerName $ComputerName -Quiet -Count 3
+        if (!($check)) { $obj | Add-Member -Name Status -Value Down -Force }
+        $colStatus += $obj
+    }
+    End { $colStatus }
+}
